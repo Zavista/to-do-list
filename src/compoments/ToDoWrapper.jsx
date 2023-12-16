643c1cae6614fedc4f8d10a17bb18fd7894f14f5
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-16 16:22:00 -0500

    updated ToDoWrapper

--- src/compoments/ToDoWrapper.jsx
+++ src/compoments/ToDoWrapper.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ToDoForm } from './ToDoForm'
+import { v4 as uuidv4 } from 'uuid';
 
 export const ToDoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    const newToDo = {
+      id: uuidv4(), 
+      task: todo, 
+      completed: false, 
+      isEditing: false
+    }
+    setTodos([...todos, newToDo]);
+  }
   return (
     <div className='TodoWrapper'>
       <ToDoForm></ToDoForm>
