550de7cf4e115376fd6dfbd7e5aa97c3009e1661
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-16 16:40:22 -0500

    imported fortawesome and created ToDo

--- src/compoments/ToDo.jsx
+++ src/compoments/ToDo.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons" //react automatically import these from fortawesome
+
 
 export const ToDo = () => {
   return (
-    <div>ToDo</div>
+    <div className='Todo'>
+      <p>Go to school</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+      </div>
+    </div>
   )
+  //FontAwesomeIcon is a React component that we imported and renders the icon that we give it
 }
