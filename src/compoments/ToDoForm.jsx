00f9e9009222be2155ee9eeb8a9917bc0fd95980
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-16 16:12:15 -0500

    Created change and submit buttons

--- src/compoments/ToDoForm.jsx
+++ src/compoments/ToDoForm.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const ToDoForm = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); //prevents the page from reloading after form submission.
+    console.log(value); //from the State value
+  }
+
   return (
-    <form className='TodoForm'>
-    <input type='text' className='todo-input' 
-    placeholder='What is the task today>'></input>
-    <button type='submit' className='todo-btn'></button>
-  </form>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type='text' 
+      className='todo-input' 
+      placeholder='What is the task today>'
+      onChange={handleChange}></input>
+      <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
   )
 }
